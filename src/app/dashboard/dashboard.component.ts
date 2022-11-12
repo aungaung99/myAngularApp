d0db251b5257a24c74b8e5c8a1be1505aa55e03a
Aung Naing Oo <aungaung20.ab@gmail.com>
2022-11-12 16:03:55 +0630

    added tables sorting

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { UsersService } from 'src/app/master/users/users.service';
+
+export interface Users {
+  id: number,
+  name: string
+}
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
+  ELEMENT_DATA: Users[] = [
+    { id: 1, name: 'Hello' },
+    { id: 2, name: 'Hllo 2' }
+  ];
+  displayedColumns: string[] = ['id', 'name'];
+  dataSource = new MatTableDataSource<Users>();
+  constructor(private user: UsersService, private _liveAnnouncer: LiveAnnouncer) { }
+  @ViewChild(MatSort) sort!: MatSort;
+  async ngOnInit() {
+    this.user.getData().subscribe(data => {
+      //this.ELEMENT_DATA = data;
+      this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+      this.dataSource.sort = this.sort;
+    });
+  }
 
-  constructor() { }
+  async ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
-  ngOnInit(): void {
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 }
--- src/app/master/users/users.component.ts
+++ src/app/master/users/users.component.ts
@@ -1,26 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Users } from 'src/app/model/users';
 import { UsersService } from './users.service';
+import { MatSort, Sort } from "@angular/material/sort";
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { MatTableDataSource } from "@angular/material/table";
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, AfterViewInit {
 
   loading = true;
-  constructor(private user: UsersService) {
+  dataSource = new MatTableDataSource<Users>();
+  displayedColumns: string[] = ['id', 'name', 'username'];
 
+  constructor(private user: UsersService, private _liveAnnouncer: LiveAnnouncer) {}
+
+  ngOnInit() {
+    this.user.getData().subscribe(data => {
+      this.dataSource = new MatTableDataSource<Users>(data);
+      this.loading = false;
+      this.dataSource.sort = this.sort;
+    });
   }
 
-  displayedColumns: string[] = ['id', 'name', 'username'];
-  user_data: Users[] = [];
-  ngOnInit(): void {
-    this.user.getData().subscribe(data => {
-      this.user_data = data;
-      this.loading = false;
-      console.log(this.user_data);
-    });
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    console.log(sortState);
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 }
