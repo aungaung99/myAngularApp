641854e56db9711451027a0173b34a1d70d43433
Aung Naing Oo <aungaung20.ab@gmail.com>
2022-11-14 03:08:33 +0630

    update services

---
+++ src/app/util/breadcrumbs.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BreadcrumbsService {
+
+  constructor() { }
+
+  helloConsole() {
+    console.log('Hello')
+  }
+}
