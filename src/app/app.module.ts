99fd632f65ef69e52c787e67225f9f5fd2137d3a
Aung Naing Oo <aungaung20.ab@gmail.com>
2022-11-16 03:03:42 +0630

    update app files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from "@angular/material/checkbox";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { MatCardModule } from "@angular/material/card";
@@ -17,7 +18,7 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatSortModule } from "@angular/material/sort";
 import { MatExpansionModule } from "@angular/material/expansion";
-
+import { DataTablesModule } from "angular-datatables";
 import { BreadcrumbModule } from "primeng/breadcrumb";
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -38,10 +39,12 @@ import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    DataTablesModule,
     MatButtonModule,
     MatToolbarModule,
     MatProgressBarModule,
     MatCardModule,
+    MatCheckboxModule,
     MatSidenavModule,
     MatIconModule,
     MatListModule,
