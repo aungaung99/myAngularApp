cbbb73078d147be047d866b95df6c38041000010
Aung Naing Oo <aungaung20.ab@gmail.com>
2022-11-16 03:02:48 +0630

    states integrate datatables

--- src/app/master/states/states.component.ts
+++ src/app/master/states/states.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Comment } from "../../model/comment";
+import { CommentService } from 'src/app/api/comment.service';
 
 @Component({
   selector: 'app-states',
   templateUrl: './states.component.html',
   styleUrls: ['./states.component.css']
 })
-export class StatesComponent implements OnInit {
+export class StatesComponent implements OnDestroy, OnInit {
+  dtOptions: DataTables.Settings = {};
+  comments: Comment[] = [];
 
-  constructor() { }
+  dtTrigger: Subject<any> = new Subject<any>();
+  constructor(private commentService: CommentService, private http: HttpClient) { }
+
+  someClickHandler(info: any): void {
+    console.log(info);
+  }
 
   ngOnInit(): void {
+
+    this.dtOptions = {
+
+      data: this.comments,
+      rowCallback: (row: Node, data: any[] | Object, index: number) => {
+        console.log(row);
+        const self = this;
+
+        $('td', row).off('click');
+        $('td', row).on('click', () => {
+          self.someClickHandler(data);
+        });
+
+        return row;
+      }
+    }
+    this.commentService.getData().subscribe(data => {
+      this.comments = data;
+      this.dtTrigger.next(data);
+    });
+  }
+
+  ngOnDestroy() {
+    this.dtTrigger.unsubscribe();
   }
 
 }
